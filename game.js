2f07dd6f31e4890645cdc8a32ebbc2773a959f9b
Jack Morice <jackkmorice@gmail.com>
2025-08-31 12:49:40 -0500

    Added placeholder pictures Split CSS into it's own file Added values to the msButton objects to be able to handle all functions of minesweeper Added logic to handle clicking, flagging, and placeholder logic for revealing tiles with number values Added comments to explain more functions

--- game.js
+++ game.js
@@ -5,11 +5,19 @@
             const resetButton = document.getElementById("resetButton");
             const gameStatus = document.getElementById("gameStatus");
             const msTiles = document.querySelectorAll("#msTile");
+
+            let bombTiles=[]; //list of tiles with bombs
+            let safeTiles=[]; //list of tiles without bombs
+            let flaggedTiles=[]; //list of flagged tiles
+
             let boardSize = 0; //Size of board
             let gameActive = 0; //0: INACTIVE | 1: ACTIVE
+        
+            //Jack Notes
+            //idea for win condition, check if flagged tiles match bomb tiles, if so, call endGame(0)?
+
 
         //UI Section
-
             //Disable Function
             function disableButton() {
                 playButton.disabled = true;
@@ -29,15 +37,14 @@
 
 
         //Gameplay Section
-
             //Load Game Function
             function loadGame() {
                 bombAmount.disabled = true; //Disable Bomb Select
                 selectButton.disabled = true; //Disable Select Button
                 playButton.disabled = true; //Disable Play Button
                 gameStatus.innerHTML = ""; //Clear the notification
-                generateBoard();
-                playGame();
+                generateBoard(); //generate the x by x board
+                playGame(); //start the actual game
             }
 
             //Play Function
@@ -49,14 +56,25 @@
                 //Left Click Listener
                 document.addEventListener('click', tileIdentify => { //used Reddit to find similar function and learn target
                     if (tileIdentify.target.matches('button')) {
-                        document.getElementById("testPara").innerHTML = tileIdentify.target.value; //test line [DELETE LATER]
-                        //FIRST CLICK HERE
-                        loadBomb()
+                        document.getElementById("testPara").innerHTML = tileIdentify.target.id; //test line [DELETE LATER]
+                        if (firstLeftClick == 0) { //check if this is the first click or not, so we can generate the bombs
+                            firstLeftClick = 1; //change flag
+                            loadBomb(); //generate all bombs on the board
+                        }
                         if (gameActive == 1) { //Check if game is active
-                            if (tileIdentify.target.value == 'b') { //Bomb Check
+                            if (tileIdentify.target.bomb == true) { //Bomb Check
+                                tileIdentify.target.classList.add('bomb'); // Add bomb image (NOTE: StackOverflow, geeksforgeeks, and MDN web docs were used for help)
                                 gameActive = 0; //Disable Game
-                                endGame(1);
+                                endGame(1); //End Game
                             }
+                            if (tileIdentify.target.bomb == false && tileIdentify.target.revealed == false) { //if the tile is not a bomb and hasn't been touched yet
+                                tileIdentify.target.classList.add('revealed-' + tileIdentify.target.value); // Add revealed image based on value
+                                tileIdentify.target.revealed = true; //Set the tile as revealed
+                            }
+                            if (flaggedTiles==bombTiles){ //Check if all bombs are flagged (Jack note - this is totally jank and just a placeholder)
+                                endGame(2); //Win game
+                            }
+                            //Otherwise, (aka if it's flagged or revealed) do nothing to it
                         }
                     }
                 });
@@ -64,13 +82,21 @@
                 document.addEventListener('contextmenu', tileIdentify => { //used Reddit to find similar function and learn target
                     if (tileIdentify.target.matches('button')) {
                         if (gameActive == 1) {
-                            document.getElementById("testPara").innerHTML = "flag"; //test line [DELETE LATER]
+                            if (tileIdentify.target.flagged == false) { //Check if tile is already flagged
+                                tileIdentify.target.flagged = true; //set the flag status to true
+                                tileIdentify.target.classList.add('flagged'); // Add flag image
+                                flaggedTiles.push(tileIdentify.target); //Add to flagged tiles
+                            } else {
+                                tileIdentify.target.flagged = false; //set the flag status to false
+                                tileIdentify.target.classList.remove('flagged'); // Remove flag image   
+                                flaggedTiles.splice(flaggedTiles.indexOf(tileIdentify.target), 1); //Remove from flagged tiles
+                            }
                         }
                     }
                 });
             }
 
-
+        //Game Logic Function
             //End Game Function
             function endGame(condition) {
                 if (condition == '1') {
@@ -82,19 +108,10 @@
                     errorPage(2);
                 }
             }
-            
-            //Check if Tile is Number Function
-            function isNumber() {
-                //define
-            }
 
             //Check if Tile is Bomb Function
             function isBomb(tile) {
-                if (tile.value == 'b') { //Tile is a bomb
-                    return true;
-                } else { //Tile is NOT a bomb
-                    return false;
-                }
+                return tile.bomb == true; //Check if tile is a bomb, if not return false
             } 
             
             //Load Bomb Function
@@ -103,8 +120,8 @@
                 while (bombCounter != 0) {
                     let randomValue = randomNumber();
                     let tile = document.getElementById("msTile-"+ randomValue);
-                    if (tile.value !== "b"){
-                        tile.value = "b";
+                    if (tile.bomb !== true){
+                        tile.bomb = true;
                     }
                     console.log(randomValue); //test line [DELETE LATER]
                     bombCounter--;
@@ -118,17 +135,19 @@
             }
 
             //Generate Board Function
-            //id needs to be assigned to buttons
             function generateBoard() {
                 let idNum = 0; //ID number
                 for (let i = 0; i < 10; i++) {
                     const msRow = document.createElement('div'); // Create a div to store buttons
                     board.appendChild(msRow); // Append the div to the board slot
                     for (let j = 0; j < 10; j++) {
-                        const msButton = document.createElement('button'); // Create buttons k times'
-                        msButton.id = "msTile-"+idNum;// assign unique ID
-                        msButton.value = idNum;
-                        // msButton.value = 'b'; //test line [DELETE LATER]
+                        const msButton = document.createElement('button'); // Create buttons k times
+                        msButton.id = "msTile-"+idNum;// assign unique ID for tracking
+                        msButton.value = 0; // Set initial value, 0 is a bomb/empty tile, and then 1-3 for amount of bombs around it
+                        msButton.bomb = false;
+                        msButton.revealed = false;
+                        msButton.flagged = false;
+                        msButton.image = null;
                         msRow.appendChild(msButton); // Append buttons to the row
                         idNum++;
                     }
@@ -149,7 +168,6 @@
                 } if (type == 2) {
                     gameStatus.innerHTML = "Error 02: Unknown Game End Condition - Use the 'Reset' Button to reload the page."
                 }
-
             }
 
             //Image Check Function
